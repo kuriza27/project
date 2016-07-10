ec45ff2a6ce502646e66b57fdc4a63fc10f75ac0
kuriza27 <cj.bulacito@gmail.com>
2016-07-10 21:55:33 +0800

    upload files

---
+++ gallery.php
@@ -0,0 +1,116 @@
+<?php include_once 'header.php'; ?>
+<div id="main-content" class="row homecontent">
+<div class="container">
+<h1>Gallery</h1>
+
+	<h4>Awareness Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/awareness_01.jpg" title="Awareness 1"><img src="/img/gallery/awareness_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/awareness_02.jpg" title="Awareness 2"><img src="/img/gallery/awareness_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/awareness_03.jpg" title="Awareness 3"><img src="/img/gallery/awareness_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/awareness_04.jpg" title="Awareness 4"><img src="/img/gallery/awareness_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/awareness_05.jpg" title="Awareness 5"><img src="/img/gallery/awareness_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/awareness_06.jpg" title="Awareness 6"><img src="/img/gallery/awareness_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>Memorial Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/memorial_01.jpg" title="Memorial 1"><img src="/img/gallery/memorial_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/memorial_02.jpg" title="Memorial 2"><img src="/img/gallery/memorial_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/memorial_03.jpg" title="Memorial 3"><img src="/img/gallery/memorial_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/memorial_04.jpg" title="Memorial 4"><img src="/img/gallery/memorial_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/memorial_05.jpg" title="Memorial 5"><img src="/img/gallery/memorial_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/memorial_06.jpg" title="Memorial 6"><img src="/img/gallery/memorial_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>Fundraising Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/fundraising_01.jpg" title="Fundraising 1"><img src="/img/gallery/fundraising_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/fundraising_02.jpg" title="Fundraising 2"><img src="/img/gallery/fundraising_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/fundraising_03.jpg" title="Fundraising 3"><img src="/img/gallery/fundraising_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/fundraising_04.jpg" title="Fundraising 4"><img src="/img/gallery/fundraising_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/fundraising_05.jpg" title="Fundraising 5"><img src="/img/gallery/fundraising_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/fundraising_06.jpg" title="Fundraising 6"><img src="/img/gallery/fundraising_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>Military Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/military_01.jpg" title="Military 1"><img src="/img/gallery/military_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/military_02.jpg" title="Military 2"><img src="/img/gallery/military_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/military_03.jpg" title="Military 3"><img src="/img/gallery/military_03_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>Religion Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/religion_01.jpg" title="Religion 1"><img src="/img/gallery/religion_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/religion_02.jpg" title="Religion 2"><img src="/img/gallery/religion_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/religion_03.jpg" title="Religion 3"><img src="/img/gallery/religion_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/religion_04.jpg" title="Religion 4"><img src="/img/gallery/religion_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/religion_05.jpg" title="Religion 5"><img src="/img/gallery/religion_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/religion_06.jpg" title="Religion 6"><img src="/img/gallery/religion_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>School Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/school_01.jpg" title="School 1"><img src="/img/gallery/school_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/school_02.jpg" title="School 2"><img src="/img/gallery/school_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/school_03.jpg" title="School 3"><img src="/img/gallery/school_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/school_04.jpg" title="School 4"><img src="/img/gallery/school_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/school_05.jpg" title="School 5"><img src="/img/gallery/school_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/school_06.jpg" title="School 6"><img src="/img/gallery/school_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>Sports/Athletics Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/sports_01.jpg" title="Sports/Athletics 1"><img src="/img/gallery/sports_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/sports_02.jpg" title="Sports/Athletics 2"><img src="/img/gallery/sports_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/sports_03.jpg" title="Sports/Athletics 3"><img src="/img/gallery/sports_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/sports_04.jpg" title="Sports/Athletics 4"><img src="/img/gallery/sports_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/sports_05.jpg" title="Sports/Athletics 5"><img src="/img/gallery/sports_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/sports_06.jpg" title="Sports/Athletics 6"><img src="/img/gallery/sports_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>Friends &amp; Family Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/family_01.jpg" title="Friends & Family 1"><img src="/img/gallery/family_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/family_02.jpg" title="Friends & Family 2"><img src="/img/gallery/family_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/family_03.jpg" title="Friends & Family 3"><img src="/img/gallery/family_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/family_04.jpg" title="Friends & Family 4"><img src="/img/gallery/family_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/family_05.jpg" title="Friends & Family 5"><img src="/img/gallery/family_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/family_06.jpg" title="Friends & Family 6"><img src="/img/gallery/family_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	<div class="clr"></div>
+	<h4>Corporate Wristbands</h4>
+	<div class="popup-gallery">
+		<a href="/img/gallery/corp_01.jpg" title="Corporate 1"><img src="/img/gallery/corp_01_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/corp_02.jpg" title="Corporate 2"><img src="/img/gallery/corp_02_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/corp_03.jpg" title="Corporate 3"><img src="/img/gallery/corp_03_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/corp_04.jpg" title="Corporate 4"><img src="/img/gallery/corp_04_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/corp_05.jpg" title="Corporate 5"><img src="/img/gallery/corp_05_tn.jpg" class="galleryimg" ></a>
+		<a href="/img/gallery/corp_06.jpg" title="Corporate 6"><img src="/img/gallery/corp_06_tn.jpg" class="galleryimg" ></a>
+	</div>
+	</div>
+	</div>
+	<script language="javascript"><!-- 
+	$(document).ready(function() {
+		$('.popup-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function(item) {
+					return item.el.attr('title') + '<small>by Reliablebands.com</small>';
+				}
+			}
+		});
+	});
+	// -->
+	</script>
+<?php include_once 'footer.php'; ?>
