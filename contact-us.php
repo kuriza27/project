1c752d115707eea61f71cf1ada5edbfc3a98419e
Justfab <cbulacito@offsourcing.com>
2016-08-27 09:11:07 +0800

    update

--- contact-us.php
+++ contact-us.php
@@ -6,7 +6,7 @@
 		<div class="contact-content">
 			<p><span class="fa fa-search">1-800-989-0440</span></p>
 			<p><span class="fa-text">sales@promotionalwristband.com</span></p>
-			<iframe width="75%" height="496" allowTransparency="true" frameborder="0" scrolling="no" style="border:none" src="http://www.emailmeform.com/builder/embed/wahxeSte471af"><a href="http://www.emailmeform.com/builder/embed/wahxeSte471af">Fill out form.</a></iframe>
+			<iframe width="100%" height="536" allowTransparency="true" frameborder="0" scrolling="no" style="border:none" src="http://www.emailmeform.com/builder/embed/wahxeSte471af"><a href="http://www.emailmeform.com/builder/embed/wahxeSte471af">Fill out form.</a></iframe>
 			<div class="clearfix"></div>		  
 		</div>
 		<div class="clearfix"></div>
