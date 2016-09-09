31d570d8b70b373900bfc10da4e7ed2cb5752c86
kuriza27 <cj.bulacito@gmail.com>
2016-09-09 18:32:49 +0800

    update
    minor content edits

--- sidebar.php
+++ sidebar.php
@@ -1,24 +1,24 @@
 <div class="half_sm" style="width: 32%; margin: 5% 0px 1% 5%;"> 
 			<div class="addl_options">
 				<h4>Message Options</h4>
-				<p> Back Message: <span class="red">(+$0.06 each)</span><br>
-					Inside Message: <span class="red">(+$0.01 each)</span><br>
-					Add Wristband Color: <span class="red">(+$20/color)</span><br>
-					Add Wristband Size: <span class="red">(+$40/size)</span><br>
-					Digital Proof: <span class="red">(+$20/order)</span>
+				<p> Back Message<br>
+					Inside Message<br>
+					Add Wristband Colo<br>
+					Add Wristband Siz<br>
+					Digital Proof
 				</p>
 				<h4>Sizes</h4>
-				<p> Toddler Size (6.5 inch): <span class="red">(+$0.10 each)</span><br>
+				<p> Toddler Size (6.5 inch)<br>
 					Youth Size (7 inch)<br>
 					Medium Size (7.5 inch)<br>
 					Adult Size (8 inch)<br>
-					Extra Large (8.5 inch): <span class="red">(+$0.10 each)</span>
+					Extra Large (8.5 inch)
 				</p>
 				<h4>Wristband Widths</h4>
-				<p> 1/4 Inch Wide: <span class="red">(Same price as 1/2 inch)</span><br>
+				<p> 1/4 Inch Wide<br>
 					1/2 Inch Wide<br>
 					1 Inch Wide<br>
-					2 Inch Wide: <span class="red">(Call or Email for pricing)</span>
+					2 Inch Wide
 				</p>
 				<h4>Colors Available</h4>
 				<p> All Colors<br>
@@ -29,14 +29,14 @@
 				<p> <a href="fonts.php">View Fonts Available</a>
 				</p>
 				<h4>Logo Cliparts Available</h4>
-				<p> <a href="cliparts.php">View Logo Cliparts Available</a> <span class="red">(+$0.08)</span>
+				<p> <a href="cliparts.php">View Logo Cliparts Available</a>
 				</p>
 				<h4>Other Add-Ons</h4>
 				<p>
 					3mm Thich Option<span class="red"></span><br>
 					Eco Friendly<span class="red"></span><br>
 					Glitters<span class="red"></span><br>
-					Individually Pack <span class="red">(+$0.08)</span><br>
+					Individually Pack<br>
 					Keychain <span class="red">(+$0.01)</span><br>
 				</p>
 				<h4>Shipping Turnaround Time</h4>
@@ -48,47 +48,3 @@
 		</div>
 		<div class="clearfix"></div>
 <!-- End conversion wristbands -->
-<h3>* Options and Add-Ons</h3>
-			<div class="col-md-4 add-ons">
-				<div class="box-thumb"><a href="assets/images/src/add-ons/3mm-thick.png" title="3mm thick option"><img src="assets/images/src/add-ons/3mm-thick.png" class="galleryimg"></a></div>
-				<div class="icon-img"><img src="assets/images/src/icon.png"/> <div class="icon-text" style="width:150px;">Available for 1/2 and 3/4 inch wristbands only.</div></div>
-				<div class="add-ons-radio">
-					
-					<h2>3mm Thick Option</h2>
-				</div>
-			</div>
-			<div class="col-md-4 add-ons">
-				<div class="box-thumb"><a href="assets/images/src/add-ons/Digital-Proof.png" title="Digital Proof"><img src="assets/images/src/add-ons/Digital-Proof.png" class="galleryimg"></a></div>
-				<div class="icon-img"><img src="assets/images/src/icon.png"/> <div class="icon-text" style="width:150px;">We'll send you a proof for approval before production begins.</div></div>
-				<div class="add-ons-radio">
-				
-					<h2>Digital Proof</h2>
-				</div>
-			</div>
-			<div class="col-md-4 add-ons">
-				<div class="box-thumb"><a href="assets/images/src/add-ons/ecofriendly.png" title="Eco Friendly"><img src="assets/images/src/add-ons/ecofriendly.png" class="galleryimg"></a></div>
-				<div class="add-ons-radio">
-					<h2>Eco Friendly</h2>
-				</div>
-			</div>
-			<div class="col-md-4 add-ons">
-				<div class="box-thumb"><a href="assets/images/src/add-ons/Glitters.png" title="Glitters"><img src="assets/images/src/add-ons/Glitters.png" class="galleryimg"></a></div>
-				<div class="add-ons-radio">
-					<h2>Glitters</h2>
-				</div>
-			</div>
-			<div class="col-md-4 add-ons">
-				<div class="box-thumb"><a href="assets/images/src/add-ons/Individual-pack.png" title="Individual pack"><img src="assets/images/src/add-ons/Individual-pack.png" class="galleryimg"></a></div>
-				<div class="icon-img"><img src="assets/images/src/icon.png"/> <div class="icon-text" style="width:200px;">Professionally sealed on biodegradable bags with clear back to see product inside.</div></div>
-				<div class="add-ons-radio">
-					<h2>Individually Pack</h2>
-				</div>
-			</div>
-			<div class="col-md-4 add-ons">
-				<div class="box-thumb"><a href="assets/images/src/add-ons/KeyChain.png" title="KeyChain"><img src="assets/images/src/add-ons/KeyChain.png" class="galleryimg"></a></div>
-				<div class="icon-img"><img src="assets/images/src/icon.png"/> <div class="icon-text" style="width:132px;"> Available for 1/2 inch wristbands only.</div></div>
-				<div class="add-ons-radio">
-					<h2>Keychain</h2>
-				</div>
-			</div>
-			<div class="clearfix"></div>
