a7da30457822d77fcb6b0fe1181ee0e82144a6ad
Justfab <cbulacito@offsourcing.com>
2016-10-06 02:18:53 +0800

    Update
    - Fix for product pages

--- product-debossed.php
+++ product-debossed.php
@@ -178,8 +178,63 @@
 			</div>
 				<div class="clearfix"></div>
 		</div>
-		<?php include "sidebar.php"; ?>
-			<div class="clearfix"></div>
+				<div class="half_sm" style="width: 32%; margin: 5% 0px 1% 5%;"> 
+			<div class="addl_options">
+				<h4>Message Options</h4> 
+				<p>Back Message<br>
+				Inside Message<br>
+				Multiple Wristband Color <br>
+				Multiple Wristband Size<br>
+				Digital Proof <br>
+				</p>
+				<h4>Sizes</h4>
+				<p> Toddler Size (6.5 inch)<br>
+					Youth Size (7 inch)<br>
+					Medium Size (7.5 inch)<br>
+					Adult Size (8 inch)<br>
+					Extra Large (8.5 inch)
+				</p>
+				<h4>Wristband Widths</h4>
+				<p> 1/4 Inch Wide<br>
+					1/2 Inch Wide<br>
+					3/4 Inch Wide<br>
+					1 Inch Wide<br>
+					1 and 1/2 Inch Wide<br>
+					2 Inch Wide
+				</p>
+				<h4>Colors Available</h4>
+				<p> All Colors<br>
+					Free PMS Pantone Color Match<br>
+					<a href="colors.php">View Pantone Chart</a>
+				</p>
+				<h4>Fonts Available</h4>
+				<p> <a href="fonts.php">View Fonts Available</a>
+				</p>
+				<h4>Logo Cliparts Available</h4>
+				<p> <a href="cliparts.php">View Logo Cliparts Available</a>
+				</p>
+				<h4>Other Add-Ons</h4>
+				<p>
+					Swirl or Tiedie type <br>
+					Segmented <br>
+					Glow in the Dark <br>
+					Camo <br>
+					Glitters<br>
+					Individual Pack <br>
+					Keychain <br>
+					Thumb Ring <br>
+					Extra Thick <br>
+					Sequential Number<br>
+				</p>
+				<h4>Shipping Turnaround Time</h4>
+				<p> Standard Shipping (10 to 14 days)<br>
+					Rush Shipping Available (7 to 9 days): <span class="red">(Quote Upon Request)</span><br>
+					Super Rush Shipping (4 to 6 days): <span class="red">(Quote Upon Request)</span>
+				</p>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+<!-- End conversion wristbands -->
 	</div><!-----End Container---->
 </div>
 	<script language="javascript"><!-- 
